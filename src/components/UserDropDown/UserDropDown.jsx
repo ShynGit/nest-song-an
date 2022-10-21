f4792945844d61a6a862601ad66f092083975589
ShynGit <huusy832gl@gmail.com>
2022-10-21 21:30:30 +0700

    Change logout alert to header

--- src/components/UserDropDown/UserDropDown.jsx
+++ src/components/UserDropDown/UserDropDown.jsx
@@ -1,4 +1,6 @@
+import { Slide } from "@mui/material";
 import React from "react";
+import { useEffect } from "react";
 import { useState } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { Link } from "react-router-dom";
@@ -6,6 +8,7 @@ import { Link } from "react-router-dom";
 import UserIcon from "../../assets/icons/user.png";
 import {
     selectUser,
+    USER_CLEAR,
     USER_LOGOUT_REQUEST,
     USER_LOGOUT_SUCCESS,
 } from "../../features/user/userSlice";
@@ -14,6 +17,7 @@ const UserDropDown = () => {
     const user = useSelector(selectUser);
     const dispatch = useDispatch();
     const [userDropDown, setUserDropDown] = useState("hidden");
+    const [logoutSuccess, setLogoutSuccess] = useState(user.isloggedOutSuccess);
 
     const handleLogout = async () => {
         const delay = (ms) => new Promise((resolve) => setTimeout(resolve, ms));
@@ -22,11 +26,41 @@ const UserDropDown = () => {
         localStorage.removeItem("cart");
         await delay(500);
         dispatch(USER_LOGOUT_SUCCESS());
+        setLogoutSuccess(true);
+        setTimeout(() => setLogoutSuccess(false), 2000);
     };
 
     return (
         <>
             <div>
+                <Slide
+                    direction="down"
+                    in={logoutSuccess}
+                    mountOnEnter
+                    unmountOnExit
+                >
+                    <div
+                        className="z-10 fixed w-fit top-20 right-14 transition-all duration-200 bg-cyan-100 rounded-lg py-5 px-6 mb-3 text-base text-cyan-700 inline-flex items-center"
+                        role="alert"
+                    >
+                        <svg
+                            aria-hidden="true"
+                            focusable="false"
+                            data-prefix="fas"
+                            data-icon="info-circle"
+                            className="w-4 h-4 mr-2 fill-current"
+                            role="img"
+                            xmlns="http://www.w3.org/2000/svg"
+                            viewBox="0 0 512 512"
+                        >
+                            <path
+                                fill="currentColor"
+                                d="M256 8C119.043 8 8 119.083 8 256c0 136.997 111.043 248 248 248s248-111.003 248-248C504 119.083 392.957 8 256 8zm0 110c23.196 0 42 18.804 42 42s-18.804 42-42 42-42-18.804-42-42 18.804-42 42-42zm56 254c0 6.627-5.373 12-12 12h-88c-6.627 0-12-5.373-12-12v-24c0-6.627 5.373-12 12-12h12v-64h-12c-6.627 0-12-5.373-12-12v-24c0-6.627 5.373-12 12-12h64c6.627 0 12 5.373 12 12v100h12c6.627 0 12 5.373 12 12v24z"
+                            ></path>
+                        </svg>
+                        Đăng xuất thành công
+                    </div>
+                </Slide>
                 <div
                     className="relative block text-left hover:cursor-pointer"
                     onMouseOver={() => setUserDropDown("inline")}
